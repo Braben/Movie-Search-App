72864fd22fe0438cf03b1c5b51560b3eef5ac84f
Delali Prince <princedelali30@gmail.com>
2024-11-03 15:32:41 +0000

    I have worked on the signUp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 // import Footer from "./components/Footer";
 // import Hero from "./components/Hero";
-import LoginPage from "./pages/LoginPage";
+// import LoginPage from "./pages/LoginPage";
 // import MovieList from "./components/MovieList";
 // import Navbar from "./components/Navbar";
 // import SearchBar from "./components/SearchBar";
 // import HomePage from "./pages/HomePage";
 
+import SignUpPage from "./components/SignUpPage";
+
 const App = () => {
   return (
     <>
-      <LoginPage />
+       <SignUpPage />
+      {/* <LoginPage /> */}
       {/* <HomePage /> */}
     </>
   );
