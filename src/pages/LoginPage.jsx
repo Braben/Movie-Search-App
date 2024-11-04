4cfec4da4c7311391208c845c9733eec28b3880d
Delali Prince <princedelali30@gmail.com>
2024-11-04 18:12:56 +0000

    Commiting the login and signUp

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -61,6 +61,7 @@ function LoginPage() {
           <button
             className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-200"
             type="submit"
+            
           >
             Sign In
           </button>
